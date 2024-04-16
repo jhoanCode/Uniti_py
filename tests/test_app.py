252539a1db46453264d60656a66df55f5b884a79
jhoan <jhoanguzman428@gmail.com>
2024-04-16 18:48:44 -0400

    test_9

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,28 @@
-import pytest
-from app import app
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+import unittest
 
-def test_index(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b'Hola, mundoo!' in response.data
+# Importa el módulo que deseas probar
+from app import MiClase
+
+class TestMiClase(unittest.TestCase):
+    def test_algo(self):
+        # Crea una instancia de MiClase y realiza tus pruebas
+        objeto = MiClase()
+        resultado = objeto.algo()
+        self.assertEqual(resultado, esperado)
+
+
+
+
+# import pytest
+# from app import app
+
+# @pytest.fixture
+# def client():
+#     with app.test_client() as client:
+#         yield client
+
+# def test_index(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
+#     assert b'Hola, mundoo!' in response.data
